a253ed92300272f17b34790514b9567c6569c02d
Andrea <andrea.klaus.tubak@gmail.com>
2020-09-02 15:24:54 +0200

    Object version mismatch solved

--- Fair-Network/src/Frame_m.cc
+++ Fair-Network/src/Frame_m.cc
@@ -1,5 +1,5 @@
 //
-// Generated file, do not edit! Created by nedtool 5.6 from Frame.msg.
+// Generated file, do not edit! Created by nedtool 5.5 from Frame.msg.
 //
 
 // Disable warnings about unused variables, empty switch stmts, etc:
