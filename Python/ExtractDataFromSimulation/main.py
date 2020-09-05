56d31bef6134b077ccc66dd1fb5fb29fd26b9cea
Francesco Ronchieri <francesco.ronchieri@imagicle.com>
2020-09-05 15:39:50 +0200

    cleaning solution

--- Python/ExtractDataFromSimulation/main.py
+++ Python/ExtractDataFromSimulation/main.py
@@ -14,35 +14,6 @@ def main():
     #saveCsvAsJsonFile("data/prova")
     return 0
 
-def transformDictionary(dictionary):
-    iterator = iter(dictionary.keys())
-    lenKeys = len(dictionary.keys())
-    data = {"numIterations": lenKeys, "iterations": [dict() for x in range(lenKeys)]}
-    iterations = data["iterations"]
-    
-    for itCounter,childKey in enumerate(iterator):
-        vectors = dictionary[childKey]["vectors"]
-        numUsers = int(dictionary[childKey]["itervars"]["nUser"])
-        actualIteration = {"numUsers": numUsers, "users": [dict() for x in range(numUsers)]}
-        iterations[itCounter] = actualIteration
-        
-        for vec in vectors:
-            userID = vec["module"]
-            start = userID.find("[")
-            finish = userID.find("]")
-            intIdUser = int(userID[start + 1 : finish])
-            aux = vec["name"].find(":")
-            vectorName = vec["name"][0:aux]
-            actualIteration["users"][intIdUser]["userID"] = intIdUser
-            actualIteration["users"][intIdUser][vectorName] = {"time": vec["time"], "value": vec["value"]}
-
-    return data
-
-
-def saveDictToJson(data, filename):
-    with open(filename, 'w') as fp:
-        json.dump(data, fp)
-
 def getThroughputMeanValue(dictionary):
     numUser = dictionary["numUsers"]
 
--- Python/ExtractDataFromSimulation/omnetDataExtractor.py
+++ Python/ExtractDataFromSimulation/omnetDataExtractor.py
@@ -17,19 +17,24 @@ def convertOmnetJson(filename):
 
 
 def convertOmnettDictionary(dictionary):
-    firstChildKey = next(iter(dictionary))
-    vectors = dictionary[firstChildKey]["vectors"]
-    numUsers = int(dictionary[firstChildKey]["itervars"]["nUser"])
-    data = {"numUsers": numUsers, "users": [dict() for x in range(numUsers)]}
-
-    for i in range(len(vectors)):
-        actualVect = vectors[i]
-        userID = actualVect["module"]
-        start = userID.find("[")
-        finish = userID.find("]")
-        intIdUser = int(userID[start + 1 : finish])
-        aux = actualVect["name"].find(":")
-        vectorName = actualVect["name"][0:aux]
-        data["users"][intIdUser]["userID"] = intIdUser
-        data["users"][intIdUser][vectorName] = {"time": actualVect["time"], "value": actualVect["value"]}
+    iterator = iter(dictionary.keys())
+    lenKeys = len(dictionary.keys())
+    data = {"numIterations": lenKeys, "iterations": [dict() for x in range(lenKeys)]}
+    iterations = data["iterations"]
+    
+    for itCounter,childKey in enumerate(iterator):
+        vectors = dictionary[childKey]["vectors"]
+        numUsers = int(dictionary[childKey]["itervars"]["nUser"])
+        actualIteration = {"numUsers": numUsers, "users": [dict() for x in range(numUsers)]}
+        iterations[itCounter] = actualIteration
+        
+        for vec in vectors:
+            userID = vec["module"]
+            start = userID.find("[")
+            finish = userID.find("]")
+            intIdUser = int(userID[start + 1 : finish])
+            aux = vec["name"].find(":")
+            vectorName = vec["name"][0:aux]
+            actualIteration["users"][intIdUser]["userID"] = intIdUser
+            actualIteration["users"][intIdUser][vectorName] = {"time": vec["time"], "value": vec["value"]}
     return data
