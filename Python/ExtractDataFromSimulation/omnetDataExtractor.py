6dbd992f2f224909d044f783d800de95a9f2f8fe
Andrea <andrea.klaus.tubak@gmail.com>
2020-09-05 18:29:08 +0200

    Create Dictionary from exported csv

--- Python/ExtractDataFromSimulation/omnetDataExtractor.py
+++ Python/ExtractDataFromSimulation/omnetDataExtractor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json
+import csv
 
 
 def parse_if_number(s):
@@ -46,4 +47,35 @@ def convertOmnettDictionary(dictionary):
             vectorName = vec["name"][0:aux]
             actualIteration["users"][intIdUser]["userID"] = intIdUser
             actualIteration["users"][intIdUser][vectorName] = {"time": vec["time"], "value": vec["value"]}
+    return data
+
+
+def checkOrCreateKey(dictionary,key):
+    if key not in dictionary:
+        dictionary[key] = dict()
+    return dictionary[key]
+
+
+def createDictFromCSV(filename):
+    #apro e leggo il file 
+    with open(filename, encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        
+        data = dict()   # preparo il dizionario con i dati dei vectors
+
+        for row in reader: # i serve solo a controllare che ci siano effettivamente solo i vector che ci aspettiamo
+            if 'vector' in row: # mi interessano solo i vectors
+
+                runID = row[0][0:row[0].find("-2020")] # id run ripulito
+                user = row[2].split(".")[1] # tolgo "FairNetwork."
+                vectorName = row[3].split(":")[0] # prendo solo il tipo di vector
+                timeValues = [float(x) for x in row[7].split(" ")] # converto una stringa di valori divisa da " " in array di float
+                valueValues = [float(x) for x in row[8].split(" ")]
+                
+                actualRun = checkOrCreateKey(data,runID) # creo o aggiungo record a dictionary dell'esecuzione i-esima 
+                actualUser = checkOrCreateKey(actualRun,user) # anche utente e i vector sono dict
+                actualVector = checkOrCreateKey(actualUser,vectorName)
+
+                actualVector["time"] = timeValues # e poi li popolo
+                actualVector["value"] = valueValues         
     return data
