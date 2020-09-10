fc50dfb8dec134119f3c52f309cf4864faf85e35
Francesco Ronchieri <francesco.ronchieri@imagicle.com>
2020-09-10 16:53:25 +0200

    Added possibility to add command parameter to runSimulations.py

--- Python/runSimulations.py
+++ Python/runSimulations.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 import configurator as cfg
+import sys
 import os
 import json
 
 def main():
+    additionalArgs = ' '.join(sys.argv[1:])
     conf = dict()
     conf = cfg.getConfiguration()
     print('Moving in simulation directory')
     simulationDir = "{projectFolder}/simulations".format(projectFolder = conf["PROJECT_FOLDER"])
     os.chdir(simulationDir)
     print("Moved into => {dir}".format(dir=os.getcwd()))
-    command = '../src/Fair-Network -u Cmdenv -c General -n "../src;." FairNetworkConf.ini -r 0'
+    command = '{projectFolder}/src/Fair-Network -u Cmdenv -c General -n "{projectFolder}/src;{projectFolder}/simulations" FairNetworkConf.ini {additionalArgs}'.format(projectFolder = conf["PROJECT_FOLDER"], additionalArgs = additionalArgs)
     print("[DEBUG] command => {command}".format(command = command))
     os.system(command)
     return
