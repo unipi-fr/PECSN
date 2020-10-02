860e18ad5ac668ca7544e40df8ebc060d2755091
Andrea <andrea.klaus.tubak@gmail.com>
2020-10-02 18:04:48 +0200

    Script factorial analysis completed

--- Python/factorialAnalysis.py
+++ Python/factorialAnalysis.py
@@ -4,6 +4,7 @@ import numpy as np
 import configurator as cfg
 import omnetDataExtractor as ode
 import omnetDataConverter as odc
+import math
 
 from omnetConfIni import OmnetConfIni
 
@@ -18,8 +19,8 @@ def factorialAnalysis():
 
     print(f"[INFO] The number of experiments should be {numberOfExperiments}")
 
-    dataFrame = generateDataFrameWithFactors(factors)
-    print(dataFrame)
+    factorDF = generateDataFrameWithFactors(factors)
+    #print(factorDF)
         
     jsonData = ode.createJsonFromCSV(filename = "data/results.csv", skipVectors = True, skipStatistics = False)
     ode.saveJsonToFile(jsonData, "debug/test.json")
@@ -29,25 +30,30 @@ def factorialAnalysis():
 
     numOfDifferentRuns = len(jsonConverted.keys())    
     if numOfDifferentRuns != numberOfExperiments:
-        print(f"[WARNING] The number of experiments should be {numberOfExperiments} but found {numOfDifferentRuns}")
+        print(f"[WARNING] The number of different experiments should be {numberOfExperiments} but found {numOfDifferentRuns}")
     else:
         print(f"[INFO] experiments extracted: ({numOfDifferentRuns})")
 
     vectorName = "userThroughputTotalStat"
 
-    dataFrame = appendResultsToDataFrame(dataFrame,jsonConverted,factors,vectorName)
+    vectorDF, repetitions = appendResultsToDataFrame(factorDF,jsonConverted,factors,vectorName)
 
-    print(dataFrame)
+    print(vectorDF)
 
-    dataFrame.to_csv("exported.csv", sep = ";")
-    #summ = 0
-    ##for i in range(numberOfExperiments):
-    #print(sum(dataFrame["nUser"] * dataFrame[vectorName]))
-    ##summ += sumValue
+    aggregateDF = calculateAggregateResult(factorDF, vectorDF, vectorName, numberOfExperiments, repetitions)
+
+    #print(aggregateDF)
+
+    joinedDF = joinDataFrames(factorDF, vectorDF, aggregateDF)
+
+    print(joinedDF)
+
+    joinedDF.to_csv("exported.csv", sep = ";")
    
     return
 
 def appendResultsToDataFrame(dataFrame, jsonConverted, factors, vectorName):
+    vectorDF = pd.DataFrame()
     numberOfFactors = len(factors)
     numberOfExperiments = 2**numberOfFactors
 
@@ -56,19 +62,23 @@ def appendResultsToDataFrame(dataFrame, jsonConverted, factors, vectorName):
 
     repetitions = jsonConverted[firstKey][vectorName]["repetitions"]
 
-    #inizialize dataframes with 0
+    vectorDF["identity"] = dataFrame["identity"]
+
+    #inizialize dataframes with NaN
     colunNameList = list()
     columnName = f"{vectorName}"
     colunNameList.append(columnName)
-    dataFrame[columnName] = float('NaN')
+    vectorDF[columnName] = float('NaN')
     for i in range(repetitions):
         columnName = f"{vectorName}[{i}]"
         colunNameList.append(columnName)
-        dataFrame[columnName] = float('NaN')
+        vectorDF[columnName] = float('NaN')
     for i in range(repetitions):
         columnName = f"{vectorName}Error[{i}]"
         colunNameList.append(columnName)
-        dataFrame[columnName] = float('NaN')
+        vectorDF[columnName] = float('NaN')
+
+    vectorDF = vectorDF.drop(["identity"], axis=1)
 
     for i in range(numberOfExperiments):
         runKey = ""
@@ -92,8 +102,8 @@ def appendResultsToDataFrame(dataFrame, jsonConverted, factors, vectorName):
         rowList.append(mean)
         rowList = rowList + values + errors
 
-        dataFrame.loc[i,colunNameList] = rowList
-    return dataFrame
+        vectorDF.loc[i,colunNameList] = rowList
+    return vectorDF, repetitions
 
 def getFactors(factors):
     '''
@@ -128,5 +138,50 @@ def generateDataFrameWithFactors(factors):
     faDataFrame.insert(0, "identity", 1)
 
     return faDataFrame
+
+def calculateAggregateResult(factorDF, vectorDF, vectorName, numberOfExperiments, repetitions):
+    aggregateDF = pd.DataFrame()
+    
+    aggregateDF = aggregateDF.reindex(["Sum", "qi", "SSx", "Variation"])
+    
+    vectorResults = [ 0 if math.isnan(x) else x for x in vectorDF[vectorName] ]
+    #print(vectorResults) 
+    sst = 0
+    for factorK in factorDF:
+        sumValue = sum(factorDF[factorK]*vectorResults)
+        meanValue = sumValue / numberOfExperiments
+        ssx = float("NaN")
+        if factorK != "identity":
+            ssx = numberOfExperiments*repetitions*(meanValue**2)
+            sst += ssx
+
+        #print(f"{sumValue}, {meanValue}, {ssx}")
+
+        aggregateDF[factorK] = [sumValue, meanValue, ssx, float("NaN")]
+    
+    sse = 0
+    for errork in [x for x in vectorDF.keys() if x.startswith(f"{vectorName}Error")]:
+        errorValues = [ 0 if math.isnan(x) else x for x in vectorDF[errork] ]
+        sumValue = sum([x*x for x in errorValues])
+        sse += sumValue
+    aggregateDF[vectorName] = [float("NaN"), float("NaN"), sse, float("NaN")]
+
+    sst += sse
+
+    for aggregateK in aggregateDF:
+        if aggregateK == "identity":
+            continue
+        ssx = aggregateDF.loc["SSx",aggregateK]
+        variation = ssx / sst
+        aggregateDF.loc["Variation", aggregateK] = variation
+
+    return aggregateDF
+
+def joinDataFrames(factorDF, vectorDF, aggregateDF):
+    joinDF = factorDF.copy()
+    joinDF = joinDF.join(vectorDF, sort = False)
+    joinDF = joinDF.append(aggregateDF, sort = False)
+
+    return joinDF
 
 main()
