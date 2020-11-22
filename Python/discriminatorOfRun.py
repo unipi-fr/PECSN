82f03fda94fb05c0d8c907266e6f4bf825421024
Francesco Ronchieri <francesco.ronchieri@imagicle.com>
2020-11-22 17:06:43 +0100

    Discriminator of run with colors

--- Python/discriminatorOfRun.py
+++ Python/discriminatorOfRun.py
@@ -13,10 +13,26 @@ INTERVAL_OF_CONFIDENCE_USED = "0.1"
 blockPerFrameFrameStat = {
     "CONFIDENCE_LEVEL": "0.1",
     "LIST_OF_LOADS":{
-        "UNLOADED": [0, 3],
-        "NORMAL": [3, 20],
-        "HIGH_LOAD": [20, 24.9999999999],
-        "SATURATED": [25,25]
+        "UNLOADED": {
+            "INTERVAL": [0, 3],
+            "COLOR": "gray"
+        },
+        "LOW_LOAD": {
+            "INTERVAL": [3, 15],
+            "COLOR": "blue"
+        },
+        "NORMAL": {
+            "INTERVAL": [15, 22],
+            "COLOR": "green"
+        },
+        "HIGH_LOAD": {
+            "INTERVAL": [22, 24.9999999999],
+            "COLOR": "yellow"
+        },
+        "SATURATED": {
+            "INTERVAL": [25, 25],
+            "COLOR": "red"
+        }
     }    
 }
 
@@ -28,12 +44,23 @@ MODES = ["General", "Binomial"]
 def main():
     factors = fa.getFactors()
     LIST_OF_STAT = conf.keys()
-    jsonConverted = odc.prepareStatisticData(csvFile = "data/resultsGeneral.csv", factors=factors, takeAllRuns = True, levelOfDetail = 4, useJsonFileIfExists = True)
-    confidenceIntervals = da.getConfidenceIntervals(jsonConverted, LIST_OF_STAT, saveToFile = True)
+    for mode in MODES:
+        jsonConverted = odc.prepareStatisticData(csvFile = f"data/results{mode}.csv", factors=factors, takeAllRuns = True, levelOfDetail = 4, useJsonFileIfExists = True)
+        confidenceIntervals = da.getConfidenceIntervals(jsonConverted, LIST_OF_STAT, saveToFile = True)
 
-    for STAT in LIST_OF_STAT:
-        createTable(factors,confidenceIntervals,STAT)
+        for STAT in LIST_OF_STAT:
+            table = createTable(factors,confidenceIntervals,STAT)
+            name = f"{mode}-{STAT}"
+            print(f" ======== {name} =======")
+            print(table)
+            table.to_excel(f'debug/{name}.xlsx', sheet_name=STAT, index = True)
+            #table_styled = table.style.apply(background_color, list_of_loads = conf[STAT]["LIST_OF_LOADS"])
+            #print(table_styled)
+            #table_styled.to_excel(f'debug/{name}_styled.xlsx', sheet_name=STAT, index = False)
     
+def background_color(val, list_of_loads):
+    color = list_of_loads[val]["COLOR"]
+    return 'color: %s' % color
 
 def createTable(factors, confidenceIntervals, statName):
     currentStat = conf[statName]
@@ -44,7 +71,7 @@ def createTable(factors, confidenceIntervals, statName):
         raise ValueError(f"More than 2 factors -> {factors} -> abort create table")
     table = inizializeTable(factors[1], factors[0])
     table = populateTable(table, confidenceIntervals, statName , listOfLoads, confidenceLevel)
-    print(table)
+    return table
     
 
 def inizializeTable(factor1, factor2):
@@ -65,7 +92,7 @@ def populateTable(table, confidenceIntervals, statName ,listOfLoads, confidenceL
         meanInterval =( runStatInterval[0] + runStatInterval[1] )/2
 
         for load in listOfLoads:
-            loadInterval = listOfLoads[load]
+            loadInterval = listOfLoads[load]["INTERVAL"]
             
             if meanInterval >= loadInterval[0] and meanInterval <= loadInterval[1]:
                 runKeySplitted = [ x+")" for x in runK.split(")")]
